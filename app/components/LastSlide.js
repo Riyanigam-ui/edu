85c268234b2ebdd26d9bb22517d8466087439ea4
Riyanigam-ui <riyann0601@gmail.com>
2024-11-12 17:25:49 +0530

    modified:   app/components/LastSlide.js 	modified:   app/components/Swiper.js 	modified:   app/styles/carousel.module.css

--- app/components/LastSlide.js
+++ app/components/LastSlide.js
@@ -90,32 +90,6 @@ export default function LastCarousel() {
                         </div>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide className={styles.swiperslide}>
-                    <div className={styles.singleevent}>
-                        <div className={styles.eventimg}>
-                            <a href='#' className={styles.aa}><Image className={styles.sli} src={slide3} alt='slides' /></a>
-                        </div>
-                        <div className={styles.eventtxt}>
-                            <a href='#' className={styles.eventcategory1}>design</a>
-                            <a href='#' className={styles.title}>Learn with these award-winning best blog collage courses</a>
-                            <p>Are you passionate about design? Explore the world of design with our award-winning collage courses. Whether you're a novice or a seasoned designer, these courses will help you hone your skills and unleash your creativity.</p>
-                            <a href='#' className={styles.rm}>Read More <FaAngleRight className={styles.chev}/></a>
-                        </div>
-                    </div>               
-                </SwiperSlide>
-                <SwiperSlide className={styles.swiperslide}>
-                    <div className={styles.singleevent}>
-                        <div className={styles.eventimg}>
-                            <a href='#' className={styles.aa}><Image className={styles.sli1} src={slide4} alt='slides' /></a>
-                        </div>
-                        <div className={styles.eventtxt}>
-                            <a href='#' className={styles.eventcategory2}>food</a>
-                            <a href='#' className={styles.title}>The Art of Baking: Mastering Delicious Pastries</a>
-                            <p>If you have a sweet tooth and a passion for baking, this blog is for you. Learn the art of baking and master the techniques to create mouthwatering pastries and desserts that will impress everyone.</p>
-                            <a href='#' className={styles.rm}>Read More <FaAngleRight className={styles.chev}/></a>
-                        </div>
-                    </div>            
-                </SwiperSlide>
       </Swiper>
     </>
   );
